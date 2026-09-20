ae2e513d1a4b01943bf64bc847413abf349c7194
Renn1805 <renan.almeida.arau@gmail.com>
2026-09-19 22:04:42 -0300

    Separação da navegação do fluxo comum do app e da tela de login/cadastro

--- app/src/main/java/com/app/taskmanager/MainActivity.kt
+++ app/src/main/java/com/app/taskmanager/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.app.taskmanager.ui.login.LoginScreen
+import com.app.taskmanager.ui.main.MainApp
 import com.app.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,53 +41,25 @@ class MainActivity : ComponentActivity() {
 fun TaskManagerApp() {
 
     val navController = rememberNavController()
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
 
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            AppDestinations.entries.forEach { destination ->
-                item(
-                    icon = {
-                        Icon(
-                            ImageVector.vectorResource(destination.icon),
-                            contentDescription = destination.label
-                        )
-                    },
-                    label = { Text(destination.label) },
-                    selected = currentRoute == destination.route,
-                    onClick = { navController.navigate(destination.route) }
-                )
-            }
-        }
+    NavHost(
+        navController = navController,
+        startDestination = "login",
     ) {
-        NavHost(
-            modifier = Modifier.padding(20.dp),
-            navController = navController,
-            startDestination = "login",
-        ){
-            composable (route = "login") {
-                LoginScreen()
-            }
-            composable (route = AppDestinations.HOME.route){
-                Text(text = "home")
-            }
-            composable (route = AppDestinations.WORKSPACES.route){
-                Text(text = "workspaces")
-            }
-            composable (route = AppDestinations.PROFILE.route){
-                Text(text = "profile")
-            }
+        composable(route = "login") {
+            LoginScreen(
+                onLoginSuccess = {
+                    navController.navigate("main") {
+                        popUpTo("login") {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+        composable (route = "main") {
+            MainApp()
         }
     }
-}
 
-enum class AppDestinations(
-    val route: String,
-    val label: String,
-    val icon: Int,
-) {
-    HOME("home","Home", R.drawable.outline_home_24),
-    WORKSPACES("workspaces","Workspaces", R.drawable.outline_workspaces_24),
-    PROFILE("profile", "Profile", R.drawable.outline_account_circle_24),
 }
---
+++ app/src/main/java/com/app/taskmanager/ui/main/MainApp.kt
@@ -0,0 +1,72 @@
+package com.app.taskmanager.ui.main
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.app.taskmanager.R
+import com.app.taskmanager.ui.login.LoginScreen
+
+@PreviewScreenSizes
+@Composable
+fun MainApp() {
+
+    val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach { destination ->
+                item(
+                    icon = {
+                        Icon(
+                            ImageVector.vectorResource(destination.icon),
+                            contentDescription = destination.label
+                        )
+                    },
+                    label = { Text(destination.label) },
+                    selected = currentRoute == destination.route,
+                    onClick = { navController.navigate(destination.route) }
+                )
+            }
+        }
+    ) {
+        NavHost(
+            modifier = Modifier.padding(20.dp),
+            navController = navController,
+            startDestination = AppDestinations.HOME.route,
+        ){
+            composable (route = AppDestinations.HOME.route){
+                Text(text = "home")
+            }
+            composable (route = AppDestinations.WORKSPACES.route){
+                Text(text = "workspaces")
+            }
+            composable (route = AppDestinations.PROFILE.route){
+                Text(text = "profile")
+            }
+        }
+    }
+}
+
+enum class AppDestinations(
+    val route: String,
+    val label: String,
+    val icon: Int,
+) {
+    HOME("home","Home", R.drawable.outline_home_24),
+    WORKSPACES("workspaces","Workspaces", R.drawable.outline_workspaces_24),
+    PROFILE("profile", "Profile", R.drawable.outline_account_circle_24),
+}
